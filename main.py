b2142ff6014c4c1f8134297e02347e27f63eb5ac
liuxinyue0610 <zgliuxinyue@163.com>
2024-04-15 17:01:24 +0900

    12345

--- main.py
+++ main.py
@@ -18,17 +18,3 @@ def cipher(text,shift_amount):
 
 def decipher(text,shift_amount):
     return"asdf"
-
-if __name__ == "__main__":
-    original_text = get_original_text()
-
-    shift_amount = get_shift_amounta()
-
-    text_without_nonletters = remove_nonletters(original_text)
-    print("Text without non-letters:", text_without_nonletters)
-
-    encrypted_text = cipher(text_without_nonletters, shift_amount)
-    print("Encrypted text:", encrypted_text)
-
-    decrypted_text = decipher(encrypted_text, shift_amount)
-    print("Decrypted text:", decrypted_text)
